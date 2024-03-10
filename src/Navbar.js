93b4a9d561428bea074e0e79940842d6846d0e0c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-10 12:37:24 +0430

    init

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,11 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return <nav>
+    <div className="nav-center">
+      
+    </div>
+  </nav>
 }
 
 export default Navbar
