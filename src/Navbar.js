cdae743a4affb54fcc9006e7b1c913d6d7516316
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-10 12:37:44 +0430

    init

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,9 @@ import logo from './logo.svg'
 const Navbar = () => {
   return <nav>
     <div className="nav-center">
-      
+      <div className="nav-header">
+        
+      </div>
     </div>
   </nav>
 }
