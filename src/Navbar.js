51583baa55817e152e61d26bb4f106f8bbb0a085
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-11 12:53:36 +0430

    init

--- src/Navbar.js
+++ src/Navbar.js
@@ -16,8 +16,8 @@ const Navbar = () => {
         <ul className='links'>
           <li><a href="#">home</a></li>
           <li><a href="#">about</a></li>
-          <li><a href="#">home</a></li>
-          <li><a href="#">home</a></li>
+          <li><a href="#">contact</a></li>
+          <li><a href="#">products</a></li>
         </ul> 
       </div> 
       <ul className="social-icons"></ul> 
