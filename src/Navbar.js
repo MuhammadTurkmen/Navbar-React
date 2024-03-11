c9c4c411c50c2deec355f7775558630c194b2add
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-11 13:11:06 +0430

    init

--- src/Navbar.js
+++ src/Navbar.js
@@ -23,8 +23,8 @@ const Navbar = () => {
         </ul> 
       </div> 
       <ul className="social-icons">
-        {social.map((icons) => {
-          const {id, url, icon} = icons
+        {social.map((socialIcon) => {
+          const {id, url, icon} = socialIcon
           return <li key={id}>
             <a href={url}>{icon}</a>
           </li>
