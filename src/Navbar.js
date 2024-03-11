c34b59507edf7bfec6905241eadf76884ba14dc3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-11 13:19:17 +0430

    init

--- src/Navbar.js
+++ src/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
           <FaBars />  
         </button>
       </div>
-      <div className="links-container show-container"> 
+      <div className="links-container"> 
         <ul className='links'>
           {links.map((link) => {
             const {id, url, text} = link
